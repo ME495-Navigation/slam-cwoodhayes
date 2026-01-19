c8d24d0a2dea9b0b482160a7877dd14fcf3bc94f
conorbot <cwoodhayes@gmail.com>
2026-01-19 17:24:46 -0600

    filled out basic converter.cpp

--- turtlelib/src/converter.cpp
+++ turtlelib/src/converter.cpp
@@ -8,7 +8,31 @@
 #include <numbers>
 
 int main() {
+    while (true) {
+        std::print("Enter an angle: <angle> <deg|rad>, (CTRL-D to exit)\n");
+        double angle;
+        double converted_angle;
+        std::string unit;
+        std::string other_unit;
 
-    auto rad = turtlelib::normalize_angle(-5.5 * std::numbers::pi);
-    std::print("rad = {}pi", rad / std::numbers::pi);
+        if (!(std::cin >> angle >> unit)) {
+            std::print("Exiting.\n");
+            break;
+        }
+        if (unit == "deg") {
+            converted_angle = turtlelib::deg2rad(angle);
+            other_unit = "rad";
+        }
+        else if (unit == "rad") {
+            converted_angle = turtlelib::rad2deg(angle);
+            other_unit = "deg";
+        }
+        else {
+            std::print("Invalid input: please enter <angle> <deg|rad>, (CTRL-D to exit)");
+        }
+
+        std::print("{} {} is {} {}.\n", angle, unit, converted_angle, other_unit);
+    }
+
+    return 0;
 }
