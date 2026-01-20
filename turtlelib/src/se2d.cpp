04a4a64eb0a278234b2891897c8e030aab745df5
conorbot <cwoodhayes@gmail.com>
2026-01-20 08:51:27 -0600

    fixed op *= implementation; wrote 2 unit tests

--- turtlelib/src/se2d.cpp
+++ turtlelib/src/se2d.cpp
@@ -8,186 +8,194 @@
 
 namespace turtlelib
 {
-    std::istream & operator>>(std::istream & is, Twist2D & tw)
-    {
-        char c = is.peek();
-        bool brackets = (c == '<');
-        
-        if (brackets) {
+std::istream & operator>>(std::istream & is, Twist2D & tw)
+{
+  char c = is.peek();
+  bool brackets = (c == '<');
+
+  if (brackets) {
             // consume '<'
-            is.get(); 
-        }
-        
-        is >> tw.omega;
-        
+    is.get();
+  }
+
+  is >> tw.omega;
+
         // Check for optional unit
         // skip whitespace
-        is >> std::ws; 
-        c = is.peek();
-        if (std::isalpha(c)) {
-            std::string unit;
-            is >> unit;
-            if (unit[0] == 'd' || unit[0] == 'D') {
-                tw.omega = normalize_angle(deg2rad(tw.omega));
-            }
-            else if (unit[0] == 'r' || unit[0] == 'R') {
+  is >> std::ws;
+  c = is.peek();
+  if (std::isalpha(c)) {
+    std::string unit;
+    is >> unit;
+    if (unit[0] == 'd' || unit[0] == 'D') {
+      tw.omega = normalize_angle(deg2rad(tw.omega));
+    } else if (unit[0] == 'r' || unit[0] == 'R') {
                 // If 'r' or 'R', already in radians
-                tw.omega = normalize_angle(tw.omega);
-            }
-            else {
-                is.setstate(std::ios::failbit);
-                return is;
-            }
-        }
-        
-        if (brackets) {
+      tw.omega = normalize_angle(tw.omega);
+    } else {
+      is.setstate(std::ios::failbit);
+      return is;
+    }
+  }
+
+  if (brackets) {
             // consume ','
-            is.get(); 
-        }
-        is >> tw.x;
-        if (brackets) {
+    is.get();
+  }
+  is >> tw.x;
+  if (brackets) {
             // consume ','
-            is.get(); 
-        }
-        is >> tw.y;
-        
-        if (brackets) {
+    is.get();
+  }
+  is >> tw.y;
+
+  if (brackets) {
             // consume '>'
-            is.get(); 
-        }
-        
-        return is;
-    }
+    is.get();
+  }
+
+  return is;
+}
 
 
-    Transform2D::Transform2D() : tw_ {}
-    {}
+Transform2D::Transform2D()
+: tw_ {}
+{}
 
-    Transform2D::Transform2D(Vector2D trans) : 
-    tw_ {0.0, trans.x, trans.y}
-    {}
+Transform2D::Transform2D(Vector2D trans)
+:tw_ {0.0, trans.x, trans.y}
+{}
 
-    Transform2D::Transform2D(double radians) : 
-    tw_ {radians, 0.0, 0.0}
-    {}
+Transform2D::Transform2D(double radians)
+:tw_ {radians, 0.0, 0.0}
+{}
 
-    Transform2D::Transform2D(Vector2D trans, double radians) :
-    tw_ {radians, trans.x, trans.y}
-    {}
+Transform2D::Transform2D(Vector2D trans, double radians)
+:tw_ {radians, trans.x, trans.y}
+{}
 
-    Point2D Transform2D::operator()(Point2D p) const
-    {
+Point2D Transform2D::operator()(Point2D p) const
+{
         // Rotate the point, then translate
-        double cos_theta = std::cos(tw_.omega);
-        double sin_theta = std::sin(tw_.omega);
-        
-        double rotated_x = p.x * cos_theta - p.y * sin_theta;
-        double rotated_y = p.x * sin_theta + p.y * cos_theta;
-        
-        return Point2D{rotated_x + tw_.x, rotated_y + tw_.y};
-    }
+  double cos_theta = std::cos(tw_.omega);
+  double sin_theta = std::sin(tw_.omega);
 
-    Vector2D Transform2D::operator()(Vector2D v) const
-    {
+  double rotated_x = p.x * cos_theta - p.y * sin_theta;
+  double rotated_y = p.x * sin_theta + p.y * cos_theta;
+
+  return Point2D{rotated_x + tw_.x, rotated_y + tw_.y};
+}
+
+Vector2D Transform2D::operator()(Vector2D v) const
+{
         // Only rotate the vector (no translation, by definition of vector transform)
-        double cos_theta = std::cos(tw_.omega);
-        double sin_theta = std::sin(tw_.omega);
+  double cos_theta = std::cos(tw_.omega);
+  double sin_theta = std::sin(tw_.omega);
 
-        return Vector2D{v.x * cos_theta - v.y * sin_theta,
-                        v.x * sin_theta + v.y * cos_theta};
-    }
+  return Vector2D{v.x * cos_theta - v.y * sin_theta,
+    v.x * sin_theta + v.y * cos_theta};
+}
 
-    Twist2D Transform2D::operator()(Twist2D v) const
-    {
-        double omega_new = v.omega;
-        double vx_new = tw_.y * v.omega + cos(tw_.omega) * v.x - sin(tw_.omega) * v.y;
-        double vy_new = -tw_.x * v.omega + sin(tw_.omega) * v.x + cos(tw_.omega) * v.y;
+Twist2D Transform2D::operator()(Twist2D v) const
+{
+  double omega_new = v.omega;
+  double vx_new = tw_.y * v.omega + cos(tw_.omega) * v.x - sin(tw_.omega) * v.y;
+  double vy_new = -tw_.x * v.omega + sin(tw_.omega) * v.x + cos(tw_.omega) * v.y;
 
-        auto out = Twist2D{normalize_angle(omega_new), vx_new, vy_new};
-        
-        return out;
-    }
+  auto out = Twist2D{normalize_angle(omega_new), vx_new, vy_new};
 
-    Transform2D Transform2D::inv() const
-    {
-        auto v = Vector2D{-tw_.x, -tw_.y};
-        return Transform2D(v, -tw_.omega);
-    }
+  return out;
+}
 
-    Transform2D & Transform2D::operator*=(const Transform2D & rhs)
-    {
-        auto twist = Twist2D{rhs.rotation(), rhs.translation().x, rhs.translation().y};
-        tw_ = (*this)(twist);
-        return *this;
-    }
+Transform2D Transform2D::inv() const
+{
+  auto v = Vector2D{-tw_.x, -tw_.y};
+  return Transform2D(v, -tw_.omega);
+}
 
-    Vector2D Transform2D::translation() const
-    {
-        return Vector2D{tw_.x, tw_.y};
-    }
+Transform2D & Transform2D::operator*=(const Transform2D & rhs)
+{
+  // Compose transforms: T1 *= T2 means T1 = T1 * T2
+  // New rotation is sum of rotations
+  double new_omega = normalize_angle(tw_.omega + rhs.rotation());
 
-    double Transform2D::rotation() const
-    {
-        return tw_.omega;
-    }
+  // New translation is: current translation + rotated rhs translation
+  Vector2D rhs_trans = rhs.translation();
+  double cos_theta = std::cos(tw_.omega);
+  double sin_theta = std::sin(tw_.omega);
 
-    std::istream & operator>>(std::istream & is, Transform2D & tf)
-    {
-        double angle, x, y;
-        char c = is.peek();
-        bool brackets = (c == '{');
-        
-        if (brackets) {
+  double new_x = tw_.x + (rhs_trans.x * cos_theta - rhs_trans.y * sin_theta);
+  double new_y = tw_.y + (rhs_trans.x * sin_theta + rhs_trans.y * cos_theta);
+
+  tw_ = Twist2D{new_omega, new_x, new_y};
+  return *this;
+}
+
+Vector2D Transform2D::translation() const
+{
+  return Vector2D{tw_.x, tw_.y};
+}
+
+double Transform2D::rotation() const
+{
+  return tw_.omega;
+}
+
+std::istream & operator>>(std::istream & is, Transform2D & tf)
+{
+  double angle, x, y;
+  char c = is.peek();
+  bool brackets = (c == '{');
+
+  if (brackets) {
             // consume '{'
-            is.get(); 
-        }
-        
-        is >> angle;
-        
+    is.get();
+  }
+
+  is >> angle;
+
         // Check for optional unit
         // skip whitespace
-        is >> std::ws; 
-        c = is.peek();
-        if (std::isalpha(c)) {
-            std::string unit;
-            is >> unit;
-            if (unit[0] == 'd' || unit[0] == 'D') {
-                angle = normalize_angle(deg2rad(angle));
-            }
-            else if (unit[0] == 'r' || unit[0] == 'R') {
+  is >> std::ws;
+  c = is.peek();
+  if (std::isalpha(c)) {
+    std::string unit;
+    is >> unit;
+    if (unit[0] == 'd' || unit[0] == 'D') {
+      angle = normalize_angle(deg2rad(angle));
+    } else if (unit[0] == 'r' || unit[0] == 'R') {
                 // If 'r' or 'R', already in radians
-                angle = normalize_angle(angle);
-            }
-            else {
-                is.setstate(std::ios::failbit);
-                return is;
-            }
-        }
-        
-        if (brackets) {
+      angle = normalize_angle(angle);
+    } else {
+      is.setstate(std::ios::failbit);
+      return is;
+    }
+  }
+
+  if (brackets) {
             // consume ','
-            is.get(); 
-        }
-        is >> x;
-        if (brackets) {
+    is.get();
+  }
+  is >> x;
+  if (brackets) {
             // consume ','
-            is.get(); 
-        }
-        is >> y;
-        
-        if (brackets) {
+    is.get();
+  }
+  is >> y;
+
+  if (brackets) {
             // consume '}'
-            is.get(); 
-        }
+    is.get();
+  }
 
-        tf = Transform2D({x, y}, angle);
-        
-        return is;
-    }
+  tf = Transform2D({x, y}, angle);
 
-    Transform2D operator*(Transform2D lhs, const Transform2D & rhs)
-    {
-        lhs *= rhs;
-        return lhs;
-    }
+  return is;
+}
+
+Transform2D operator*(Transform2D lhs, const Transform2D & rhs)
+{
+  lhs *= rhs;
+  return lhs;
+}
 }
--- turtlelib/test/test_se2d.cpp
+++ turtlelib/test/test_se2d.cpp
@@ -42,7 +42,7 @@ TEST_CASE("test constructors")
     double rotate {1};
     turtlelib::Transform2D tf2(rotate);
     REQUIRE(tf2.rotation() == 1);
-    REQUIRE(tf2.translation().x == 0.0); 
+    REQUIRE(tf2.translation().x == 0.0);
     REQUIRE(tf2.translation().y == 0.0);
 
     // Check trans constructor
@@ -51,12 +51,12 @@ TEST_CASE("test constructors")
     vc.y = 2;
     turtlelib::Transform2D tf3(vc);
     REQUIRE(tf3.rotation() == 0);
-    REQUIRE(tf3.translation().x == 1); 
+    REQUIRE(tf3.translation().x == 1);
     REQUIRE(tf3.translation().y == 2);
     // Check rot and trans
     turtlelib::Transform2D tf4(vc, rotate);
     REQUIRE(tf4.rotation() == 1);
-    REQUIRE(tf4.translation().x == 1); 
+    REQUIRE(tf4.translation().x == 1);
     REQUIRE(tf4.translation().y == 2);
 }
 
@@ -88,7 +88,7 @@ TEST_CASE("Transform a point")
     // from this point, translate and rotate
     turtlelib::Transform2D tf5(oo, rot);
     auto pt5 = tf5(pt4);
-    REQUIRE(pt5.x == 1+std::sqrt(2));
+    REQUIRE(pt5.x == 1 + std::sqrt(2));
     REQUIRE(pt5.y == -1);
 }
 
@@ -134,7 +134,7 @@ TEST_CASE("Transform a vector")
         turtlelib::Transform2D tf(oo, rot);
         turtlelib::Vector2D vc {1, 1};
         auto vc2 = tf(vc);
-        
+
         REQUIRE_THAT(vc2.x, WithinAbs(0.0, 0.00001));
         REQUIRE_THAT(vc2.y, WithinAbs(std::sqrt(2), 0.00001));
     }
@@ -143,13 +143,13 @@ TEST_CASE("Transform a vector")
 // ############## End_Citation[0] ####################
 
 TEST_CASE("Transform a twist") {
-    auto v = Vector2D{1,2};
+    auto v = Vector2D{1, 2};
     auto tf = Transform2D(v, std::numbers::pi);
 
     SECTION("Transform a null twist.") {
         // should do nothing
         auto tw = Twist2D();
-        
+
         auto out = tf(tw);
         REQUIRE(out.x == 0);
         REQUIRE(out.y == 0);
@@ -203,7 +203,55 @@ TEST_CASE("Transform2D operator>>")
         REQUIRE(!stream.fail());
         REQUIRE(tf.translation().x == 1.0);
         REQUIRE(tf.translation().y == 2.0);
-        REQUIRE_THAT(tf.rotation(),  WithinAbs(3.14, 0.01));
+        REQUIRE_THAT(tf.rotation(), WithinAbs(3.14, 0.01));
     }
 
+}
+
+TEST_CASE("Transform2D operator*")
+{
+    SECTION("Multiply two transforms") {
+        Vector2D trans1{2.0, 0.0};
+        double angle1 = deg2rad(90);
+        Transform2D tf1(trans1, angle1);
+
+        Vector2D trans2{1.0, 0.0};
+        double angle2 = deg2rad(45);
+        Transform2D tf2(trans2, angle2);
+
+        // Result of tf1 * tf2
+        Transform2D result = tf1 * tf2;
+
+        // Verify result is correct
+        REQUIRE_THAT(result.rotation(), WithinAbs(deg2rad(135), 0.00001));
+        REQUIRE_THAT(result.translation().x, WithinAbs(2.0, 0.00001));
+        REQUIRE_THAT(result.translation().y, WithinAbs(1.0, 0.00001));
+    }
+
+    SECTION("operator* does not modify operands") {
+        Vector2D trans1{1.0, 2.0};
+        double angle1 = deg2rad(30);
+        Transform2D tf1(trans1, angle1);
+
+        Vector2D trans2{3.0, 4.0};
+        double angle2 = deg2rad(60);
+        Transform2D tf2(trans2, angle2);
+
+        // Store original values
+        Vector2D orig_trans1 = tf1.translation();
+        double orig_angle1 = tf1.rotation();
+        Vector2D orig_trans2 = tf2.translation();
+        double orig_angle2 = tf2.rotation();
+
+        // Perform multiplication
+        Transform2D result = tf1 * tf2;
+
+        // Verify operands were not modified
+        REQUIRE_THAT(tf1.translation().x, WithinAbs(orig_trans1.x, 0.00001));
+        REQUIRE_THAT(tf1.translation().y, WithinAbs(orig_trans1.y, 0.00001));
+        REQUIRE_THAT(tf1.rotation(), WithinAbs(orig_angle1, 0.00001));
+        REQUIRE_THAT(tf2.translation().x, WithinAbs(orig_trans2.x, 0.00001));
+        REQUIRE_THAT(tf2.translation().y, WithinAbs(orig_trans2.y, 0.00001));
+        REQUIRE_THAT(tf2.rotation(), WithinAbs(orig_angle2, 0.00001));
+    }
 }
