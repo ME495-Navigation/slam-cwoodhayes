823fc52cfa711183fac4731623fa3bb8c1dceef1
conorbot <cwoodhayes@gmail.com>
2026-01-22 00:22:04 -0600

    added timestep. cleaned up node

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -5,40 +5,47 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/u_int64.hpp"
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node
+/// @brief Simulator node.
+class NUSimulator : public rclcpp::Node
 {
 public:
-    MinimalPublisher()
-        : Node("minimal_publisher"), count_(0)
+    /// @brief Node constructor 
+    NUSimulator()
+        : Node("nusimulator"), count_(0)
     {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
+        param_desc.description = "Sim rate in Hz";
+        this->declare_parameter("rate", 100.0, param_desc);
+        double rate = this->get_parameter("rate").as_double();
+        
+        publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
         timer_ = this->create_wall_timer(
-            500ms, std::bind(&MinimalPublisher::timer_callback, this));
+            std::chrono::duration<double>(1.0 / rate), 
+            std::bind(&NUSimulator::timer_callback, this));
+        
+        RCLCPP_INFO(this->get_logger(), "nusimulator node constructed.");
     }
 
 private:
     void timer_callback()
     {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, world! " + std::to_string(count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        auto message = std_msgs::msg::UInt64();
+        message.data = count_++;
         publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
     size_t count_;
 };
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MinimalPublisher>());
+    rclcpp::spin(std::make_shared<NUSimulator>());
     rclcpp::shutdown();
     return 0;
 }
