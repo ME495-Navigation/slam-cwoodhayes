8ef442f466f7fcc16ea050eb0782eb1d22450148
conorbot <cwoodhayes@gmail.com>
2026-02-09 21:23:06 -0600

    wrote basic sensor data test for turtle_control

--- turtle_control/integration_test/turtle_control_test.cpp
+++ turtle_control/integration_test/turtle_control_test.cpp
@@ -19,7 +19,7 @@ using namespace std::chrono_literals;
 // ####################### Begin_Citation[11] ##################### --- IGNORE ---
 // The following code is adapted from the example integration test provided in the catch_ros2 package:
 
-TEST_CASE("example_integration_test", "[integration]") {
+TEST_CASE("turtle_control test - cmd_vel to wheel_cmd", "[integration]") {
   // Create a simple client node to check if the auxiliary node
   // has a service available
   auto node = rclcpp::Node::make_shared("integration_test_node");
@@ -110,5 +110,73 @@ TEST_CASE("example_integration_test", "[integration]") {
     auto received_cmd = wheel_cmd_recv_queue->front();
     CHECK_THAT(received_cmd->left_velocity, Catch::Matchers::WithinAbs(-received_cmd->right_velocity, 3)); 
   }
+
+  // test that verifies that encoder data on sensors is converted to joint_states properly
 }
 // ####################### End_Citation[11] ##################### --- IGNORE ---
+
+TEST_CASE("turtle_control test - sensor_data to joint_states", "[integration]") {
+  auto node = rclcpp::Node::make_shared("integration_test_node");
+
+  node->declare_parameter<double>("test_duration", 2.0);
+  const auto test_duration =
+    node->get_parameter("test_duration").get_parameter_value().get<double>();
+
+  auto sensor_data_pub = node->create_publisher<nuturtlebot_msgs::msg::SensorData>("sensor_data", 10);
+  auto joint_state_recv_queue = std::make_shared<std::queue<sensor_msgs::msg::JointState::SharedPtr>>();
+  auto joint_state_sub = node->create_subscription<sensor_msgs::msg::JointState>(
+    "joint_states", 10,
+    [joint_state_recv_queue](const sensor_msgs::msg::JointState::SharedPtr msg) {
+      RCLCPP_INFO(
+        rclcpp::get_logger("integration_test_node"),
+        "Received joint state with %zu positions", msg->position.size());
+      joint_state_recv_queue->push(msg);
+    });
+
+  auto wait_for_subs_and_pubs = [&]() {
+    const auto timeout = std::chrono::duration<double>(test_duration);
+    auto wait_start = std::chrono::steady_clock::now();
+    auto wait_rate = rclcpp::WallRate(50ms);
+
+    while (sensor_data_pub->get_subscription_count() == 0 ||
+           joint_state_sub->get_publisher_count() == 0) {
+      if (std::chrono::steady_clock::now() - wait_start > timeout) {
+        FAIL("Timed out waiting for pub/sub matching on sensor_data or joint_states");
+      }
+      rclcpp::spin_some(node);
+      wait_rate.sleep();
+    }
+  };
+
+  auto wait_for_joint_state = [&]() {
+    const auto timeout = std::chrono::duration<double>(test_duration);
+    auto wait_start = std::chrono::steady_clock::now();
+    auto wait_rate = rclcpp::WallRate(50ms);
+
+    while (joint_state_recv_queue->empty()) {
+      if (std::chrono::steady_clock::now() - wait_start > timeout) {
+        FAIL("Timed out waiting for joint_states message");
+      }
+      rclcpp::spin_some(node);
+      wait_rate.sleep();
+    }
+  };
+
+  SECTION("sensor_data -> joint_states") {
+    wait_for_subs_and_pubs();
+
+    auto sensor = nuturtlebot_msgs::msg::SensorData();
+    sensor.left_encoder = 100;
+    sensor.right_encoder = 200;
+    sensor_data_pub->publish(sensor);
+
+    wait_for_joint_state();
+
+    // basic checks
+    CHECK(joint_state_recv_queue->size() == 1);
+    const auto received_state = joint_state_recv_queue->front();
+    CHECK(received_state->position.size() >= 2);
+    CHECK(received_state->name.size() >= 2);
+    CHECK(received_state->position.at(0) != received_state->position.at(1));
+  }
+}
