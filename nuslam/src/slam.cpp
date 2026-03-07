4a23b04ece66afc9932826e7514d09e9d283199b
conorbot <cwoodhayes@gmail.com>
2026-03-07 11:50:11 -0600

    wiring close to done. just missing wheel tfs

--- nuslam/src/slam.cpp
+++ nuslam/src/slam.cpp
@@ -1,5 +1,5 @@
 /// \file
-/// \brief contains odometry node.
+/// \brief contains SLAM node (based on odometry node in turtle_control).
 
 #include "nav_msgs/msg/odometry.hpp"
 #include "nav_msgs/msg/path.hpp"
@@ -23,7 +23,7 @@
 #include <format>
 #include <queue>
 
-/// \brief Node for odometry estimation of the robot.
+/// \brief Node for SLAM + odometry estimation of the robot.
 ///
 /// Subscribes:
 /// - joint_states (sensor_msgs/msg/JointState): Current wheel joint positions
@@ -31,12 +31,15 @@
 /// Publishes:
 /// - odom (nav_msgs/msg/Odometry): Robot odometry pose and twist
 /// - blue/path (nav_msgs/msg/Path): Robot path based on odometry measurements
+/// - green/path (nav_msgs/msg/Path): Robot path based on SLAM pose estimates
 ///
 /// Services:
 /// - set_initial_pose (turtle_control/srv/SetPose): Sets the initial pose for odometry
 ///
 /// Broadcasts:
+/// - map -> odom transform via tf2 (for SLAM pose estimates)
 /// - odom -> body_id transform via tf2 (ie odom -> blue base_footprint)
+/// - odom -> slam_body_id transform via tf2 (ie odom -> green base_footprint)
 class Odometry : public rclcpp::Node
 {
 public:
@@ -51,6 +54,7 @@ public:
 
     odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("odom", qos);
     path_pub_ = create_publisher<nav_msgs::msg::Path>("blue/path", qos);
+    slam_path_pub_ = create_publisher<nav_msgs::msg::Path>("green/path", qos);
 
     initial_pose_srv_ = create_service<turtle_control::srv::SetPose>(
       "set_initial_pose",
@@ -68,6 +72,11 @@ public:
       auto desc = rcl_interfaces::msg::ParameterDescriptor();
       desc.description = "Name of the body frame of the robot";
       declare_parameter("body_id", "base_footprint", desc);
+    }
+    {
+      auto desc = rcl_interfaces::msg::ParameterDescriptor();
+      desc.description = "Name of the body frame of the SLAM estimate";
+      declare_parameter("slam_body_id", "base_footprint_slam", desc);
     }
     {
       auto desc = rcl_interfaces::msg::ParameterDescriptor();
@@ -102,8 +111,11 @@ public:
     }
     body_id_ = get_parameter("body_id").as_string();
     odom_id_ = get_parameter("odom_id").as_string();
+    map_id_ = "map"; 
+    world_id_ = "nusim/world";
     wheel_left_ = get_parameter("wheel_left").as_string();
     wheel_right_ = get_parameter("wheel_right").as_string();
+    slam_body_id_ = get_parameter("slam_body_id").as_string();
 
     wheel_radius_ = get_parameter("wheel_radius").as_double();
     track_width_ = get_parameter("track_width").as_double();
@@ -119,7 +131,7 @@ public:
     tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(this);
 
     // publish an initial transform at the origin so that we have a valid tf as soon as possible
-    publish_pose_tf(turtlelib::Transform2D());
+    publish_pose_tf(turtlelib::Transform2D(), world_id_, body_id_);
 
     RCLCPP_INFO(get_logger(), "odometry node constructed.");
   }
@@ -182,37 +194,64 @@ private:
 
     // publish odometry msg and tf
     odom_pub_->publish(odom_msg);
-    publish_pose_tf(T_ob);
+    publish_pose_tf(T_ob, world_id_, body_id_);
 
-    // publish path with max 500 poses
+    // TODO get slam data so this can be fr; for now just re-publish odometry info for slam
+    publish_pose_tf(T_ob, odom_id_, slam_body_id_);
+
+    turtlelib::Transform2D T_om = T_ob; // placeholder for actual SLAM pose estimate (transform from odom to map)
+    publish_pose_tf(T_om, map_id_, odom_id_);
+
+    publish_path(
+      msg->header.stamp,
+      odom_id_,
+      odom_msg.pose.pose,
+      slam_path_buffer_,
+      slam_path_pub_);
+
+    publish_path(
+      msg->header.stamp,
+      world_id_,
+      odom_msg.pose.pose,
+      path_buffer_,
+      path_pub_);
+  }
+
+  void publish_path(
+    const builtin_interfaces::msg::Time & stamp,
+    const std::string & frame_id,
+    const geometry_msgs::msg::Pose & pose,
+    std::deque<geometry_msgs::msg::PoseStamped> & path_buffer,
+    const rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr & path_publisher)
+  {
     auto pose_stamped = geometry_msgs::msg::PoseStamped();
-    pose_stamped.header.stamp = msg->header.stamp;
-    pose_stamped.header.frame_id = odom_id_;
-    pose_stamped.pose = odom_msg.pose.pose;
+    pose_stamped.header.stamp = stamp;
+    pose_stamped.header.frame_id = frame_id;
+    pose_stamped.pose = pose;
 
-    path_buffer_.push_back(pose_stamped);
-    if (path_buffer_.size() > max_path_size_) {
-      path_buffer_.pop_front();
+    path_buffer.push_back(pose_stamped);
+    if (path_buffer.size() > max_path_size_) {
+      path_buffer.pop_front();
     }
 
     auto path_msg = nav_msgs::msg::Path();
-    path_msg.header.stamp = msg->header.stamp;
-    path_msg.header.frame_id = odom_id_;
+    path_msg.header.stamp = stamp;
+    path_msg.header.frame_id = frame_id;
     path_msg.poses = std::vector<geometry_msgs::msg::PoseStamped>(
-        path_buffer_.begin(), path_buffer_.end());
-    path_pub_->publish(path_msg);
+      path_buffer.begin(), path_buffer.end());
+    path_publisher->publish(path_msg);
   }
 
-  void publish_pose_tf(const turtlelib::Transform2D & T_ob)
+  void publish_pose_tf(const turtlelib::Transform2D & T, const std::string & parent_frame, const std::string & child_frame)
   {
-    const auto quat = turtlelib::angle_to_2d_planar_quaternion(T_ob.rotation());
+    const auto quat = turtlelib::angle_to_2d_planar_quaternion(T.rotation());
     auto tf = geometry_msgs::msg::TransformStamped();
     tf.header.stamp = get_clock()->now();
-    tf.header.frame_id = odom_id_;
-    tf.child_frame_id = body_id_;
+    tf.header.frame_id = parent_frame;
+    tf.child_frame_id = child_frame;
 
-    tf.transform.translation.x = T_ob.translation().x;
-    tf.transform.translation.y = T_ob.translation().y;
+    tf.transform.translation.x = T.translation().x;
+    tf.transform.translation.y = T.translation().y;
     tf.transform.translation.z = 0.0;
     tf.transform.rotation.x = quat[0];
     tf.transform.rotation.y = quat[1];
@@ -233,7 +272,7 @@ private:
 
     turtlelib::Transform2D new_pose({request->x, request->y}, request->theta);
     diff_drive_->reset_to_configuration(new_pose);
-    publish_pose_tf(new_pose);
+    publish_pose_tf(new_pose, world_id_, body_id_);
     path_buffer_.clear();
     response->success = true;
   }
@@ -241,13 +280,18 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_sub_;
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr slam_path_pub_;
   rclcpp::Service<turtle_control::srv::SetPose>::SharedPtr initial_pose_srv_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   std::unique_ptr<turtlelib::DiffDrive> diff_drive_;
   std::deque<geometry_msgs::msg::PoseStamped> path_buffer_;
+  std::deque<geometry_msgs::msg::PoseStamped> slam_path_buffer_;
   size_t max_path_size_;
   std::string body_id_;
   std::string odom_id_;
+  std::string slam_body_id_;
+  std::string map_id_;
+  std::string world_id_;
   std::string wheel_left_;
   std::string wheel_right_;
   double wheel_radius_;
